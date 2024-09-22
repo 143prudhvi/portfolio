abfb905ba39a8636b1e3ad22c0c07ac2049217ae
Prudhvi <prudhvinath1998@gmail.com>
2024-09-22 17:25:03 -0500

    Updated Education

--- src/components/education-component/education-component.jsx
+++ src/components/education-component/education-component.jsx
@@ -11,26 +11,6 @@ const EducationComponent = () => {
                 <div className='education-cotainer right'>
                     <div className='education-content'>
                         <div className='education-details'>
-                            <h1 className='education-degree'>Bachelor's of Science in Computer Science</h1>
-                            <div className='education-university'>Dr. MGR University</div>
-                            <div className='education-year'><strong>Aug 2016</strong> - <strong>May 2020</strong></div>
-                            <div className='education-grade'>GPA <strong>3.6</strong></div>
-                            <ul className='education-coursework'>
-                                <li>OOPs</li>
-                                <li>Operating Systems</li>
-                                <li>Computer Networks</li>
-                                <li>Image Processing</li>
-                                <li>Database Management</li>
-                            </ul>
-                        </div>
-                        <div className='education-logo'>
-                            <img src={MGRLogo} />
-                        </div>
-                    </div>
-                </div>
-                <div className='education-cotainer right'>
-                    <div className='education-content'>
-                        <div className='education-details'>
                             <h1 className='education-degree'>Master's of Science in Computer and Information Sciences</h1>
                             <div className='education-university'>Wichita State University</div>
                             <div className='education-year'><strong>Jan 2023</strong> - <strong>Dec 2024</strong></div>
@@ -45,6 +25,26 @@ const EducationComponent = () => {
                         </div>
                         <div className='education-logo'>
                             <img src={WichitaLogo} />
+                        </div>
+                    </div>
+                </div>
+                <div className='education-cotainer right'>
+                    <div className='education-content'>
+                        <div className='education-details'>
+                            <h1 className='education-degree'>Bachelor's of Science in Computer Science</h1>
+                            <div className='education-university'>Dr. MGR University</div>
+                            <div className='education-year'><strong>Aug 2016</strong> - <strong>May 2020</strong></div>
+                            <div className='education-grade'>GPA <strong>3.6</strong></div>
+                            <ul className='education-coursework'>
+                                <li>OOPs</li>
+                                <li>Operating Systems</li>
+                                <li>Computer Networks</li>
+                                <li>Image Processing</li>
+                                <li>Database Management</li>
+                            </ul>
+                        </div>
+                        <div className='education-logo'>
+                            <img src={MGRLogo} />
                         </div>
                     </div>
                 </div>
