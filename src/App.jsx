daeb0574e834660753efcf7e3973c7cf5a4f8b95
Prudhvi <prudhvinath1998@gmail.com>
2024-09-22 01:36:45 -0500

    Jenkins Check

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss'
 import EducationComponent from './components/education-component/education-component'
+import ExperienceComponent from './components/experience-component/experience-component'
 import IntroComponent from './components/intro-component/intro-component'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <IntroComponent />
       <EducationComponent />
+      <ExperienceComponent />
     </>
   )
 }
---
+++ src/components/experience-component/experience-component.jsx
@@ -0,0 +1,11 @@
+import './experience-component.scss';
+
+const ExperienceComponent = () => {
+    return (
+        <div className='experience'>
+            About Experience
+        </div>
+    )
+}
+
+export default ExperienceComponent;
