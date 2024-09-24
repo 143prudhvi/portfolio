c5dc26fe2125897a2899a2555f79439209d4a1a6
Prudhvi <prudhvinath1998@gmail.com>
2024-09-24 08:10:48 -0500

    Responsiveness

--- src/components/skills-component/skills-component.jsx
+++ src/components/skills-component/skills-component.jsx
@@ -2,7 +2,7 @@ import './skills-component.scss';
 
 const SkillsComponents = () => {
     return (
-        <div className='skills'>
+        <div id='skills' className='skills'>
             <h1 className='section-title'>Skills</h1>
         </div>
     )
