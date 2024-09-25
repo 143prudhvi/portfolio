b9279a2be116d370c41b9d371a70e6f8e0173124
Prudhvi <prudhvinath1998@gmail.com>
2024-09-25 09:39:19 -0500

    Status Bar

--- src/components/intro-component/intro-component.jsx
+++ src/components/intro-component/intro-component.jsx
@@ -3,19 +3,23 @@ import React from 'react'
 import Logo from './../../assets/Images/ProfilePic.jpg'
 const IntroComponent = () => {
     return (
-        <div className='intro'>
-            <div className='profile-pic-outer-ring'>
-                <div className='profile-pic-inner-ring'>
-                    <img src={Logo} />
+        <div>
+            <div className='status-bar'>This portfolio is currently under development, with regular updates being added day by day. Please check back for new content and improvements!</div>
+            <div className='intro'>
+                <div className='profile-pic-outer-ring'>
+                    <div className='profile-pic-inner-ring'>
+                        <img src={Logo} />
+                    </div>
                 </div>
+                <h1>Software Developer</h1>
+                <span className='description'>A dedicated software developer with expertise in designing and building scalable applications. 
+                    Skilled in multiple programming languages and frameworks, with a strong focus on clean code and innovative solutions.
+                    Passionate about solving complex problems and optimizing system performance to deliver seamless user experiences.
+                </span>
+                <button className='btn-resume-download'>Download Resume</button>
             </div>
-            <h1>Software Developer</h1>
-            <span className='description'>A dedicated software developer with expertise in designing and building scalable applications. 
-                Skilled in multiple programming languages and frameworks, with a strong focus on clean code and innovative solutions.
-                Passionate about solving complex problems and optimizing system performance to deliver seamless user experiences.
-            </span>
-            <button className='btn-resume-download'>Download Resume</button>
         </div>
+        
     )
 }
 
